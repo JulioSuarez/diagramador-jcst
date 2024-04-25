9fcbf365ca0cb5a49fdddbfeb741c205e31534ea
IvanaLengstorff <ivanalengstorff27@gmail.com>
2024-04-25 02:02:23 -0400

    Update navigation-menu.blade.php

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -17,7 +17,7 @@
                         {{ __('Home') }}
                     </x-nav-link>
                     <x-nav-link href="{{ route('diagramador.index') }}" :active="request()->routeIs('diagramador.index')">
-                        {{ __('Mis Diagramas') }}
+                        {{ __('Mis Diagramas I') }}
                     </x-nav-link>
                 </div>
             </div>
