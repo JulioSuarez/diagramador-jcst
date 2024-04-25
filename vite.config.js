9eefd96ddbfa4ea8bd57445af2afc813c828720d
Julio Suarez <julio.suarez91@gmail.com>
2024-04-25 00:54:58 -0400

    assets

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
 import { defineConfig } from 'vite';
+import laravel, { refreshPaths } from 'laravel-vite-plugin';
+import glob from 'glob';
 
+// Obtén la lista de archivos JS en el directorio resources/js
+const jsFiles = glob.sync('resources/js/*.js');
+const cssFiles = glob.sync('resources/css/*.css');
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/js/bootstrap.js',
-                'resources/js/custom-modal.js',
-                'resources/js/diagramador.js',
-                'resources/js/diagramador.js',
-                'resources/js/modal-link.js',
-                'resources/js/socket-client.js'
+                ...cssFiles, // Agrega todos los archivos css del directorio
+                ...jsFiles, // Agrega todos los archivos JS del directorio
             ],
             refresh: [
+                ...refreshPaths,
                 'app/Livewire/**',
             ],
         }),
