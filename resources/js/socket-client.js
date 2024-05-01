cd58c99677e23e425b28da82bd84a06c9aabe462
Julio Suarez <julio.suarez91@gmail.com>
2024-05-01 01:11:28 -0400

    asset

--- resources/js/socket-client.js
+++ resources/js/socket-client.js
@@ -1,25 +1,32 @@
-const { io } = require('socket.io-client');
+import { io } from 'socket.io-client';
+// import go from 'gojs';
 
-const socket = io('http://3.15.228.59:3000', {
+// export const socket = io('http://172.31.21.125:3000', {
+// export const socket = io('http://127.0.0.1:3000', {
+// export const socket = io('http://0.0.0.0:3000', {
+// export const socket = io('http://3.142.249.222:3000', {
+export const socket = io('http://3.15.228.59:3000', {
+// export const socket = io('http://juliosuarez.tech:3000', {
     transports: ['websocket'],
 });
 
+
 socket.on('connect', function () {
-    console.log('Conectado con el servidor');
+    console.log('conectado con el servidor');
     socket.emit('getGuestCount');
 });
 
 socket.on('guestCount', (count) => {
     console.log('Número de invitados en la sala:', count);
-    // Pasar count a la vista
+    //pasar count a la vista
     // document.getElementById('guestCount').innerText = count;
 });
 
-// Suponiendo que 'id_user' está definido previamente
-const user = 'id_user'; // Simplemente para fines de prueba
+
+const user = document.getElementById('id_user').value;
 socket.emit('saludo', user);
 
-// Recibo el saludo del servidor y lo muestro en consola
+// recibo el saludo del servidor y lo muestro en consola
 socket.on('saludo_respuesta', (respuesta) => {
     console.log(respuesta);
 });
